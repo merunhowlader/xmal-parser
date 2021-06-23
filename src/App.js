650fbeb84f551d9b39531cc65a0b36301f1e8bf4
merunhowlader <merunhowlader@gmail.com>
2021-06-24 00:00:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import XLSX from 'xlsx';
+import { make_cols } from './MakeColumns';
+import { SheetJSFT } from './types';
+import { useState } from 'react';
 
 function App() {
+
+  const [cols, setCols] = useState([]);
+  const [data, setData] = useState([]);
+  const [file, setFile] = useState({});
+
+  function handleChange(e) {
+    const files = e.target.files;
+
+    if (files && files[0]) {
+      setFile(files[0]);
+    }
+  }
+
+  function handleFile() {
+    /* Boilerplate to set up FileReader */
+    const reader = new FileReader();
+    const rABS = !!reader.readAsBinaryString;
+
+    reader.onload = e => {
+      /* Parse data */
+      const bstr = e.target.result;
+      const wb = XLSX.read(bstr, {
+        type: rABS ? 'binary' : 'array',
+        bookVBA: true
+      });
+      /* Get first worksheet */
+
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+
+      const data = XLSX.utils.sheet_to_json(ws);
+      /* Update state */
+
+      setData(data);
+      setCols(make_cols(ws['!ref']));
+      
+        // console.log(JSON.stringify(data, null, 2));
+        console.log(cols);
+      
+    };
+
+    if (rABS) {
+      reader.readAsBinaryString(file);
+    } else {
+      reader.readAsArrayBuffer(file);
+    }
+
+    ;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <label htmlFor="file">Upload an excel to Process Triggers</label>
+        <br />
+        <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={handleChange} />
+        <br />
+        <input type='submit' value="Process Triggers" onClick={handleFile} />
     </div>
   );
 }
