dd9360029642ea21ead987790977e7c9535a2535
merunhowlader <merunhowlader@gmail.com>
2021-06-26 23:20:21 +0600

    second update

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import { useState } from 'react';
 
 function App() {
 
-  const [cols, setCols] = useState([]);
+  //const [cols, setCols] = useState([]);
   const [data, setData] = useState([]);
-  const [file, setFile] = useState({});
+  const [file, setFile] = useState();
 
-  function handleChange(e) {
-    const files = e.target.files;
+  // function handleChange(e) {
+  //   const files = e.target.files;
+  //   console.log(files[0]);
 
-    if (files && files[0]) {
-      setFile(files[0]);
-    }
-  }
+  //   // if (files && files[0]) {
+  //   //   setFile(files[0]);
+  //   // }
+  // }
 
   function handleFile() {
+    const bb = document.querySelector('input').files[0];
+    console.log(bb);
+    
+      setFile(document.querySelector('input').files[0]);
+    
+   // console.log(Files[0]);
     /* Boilerplate to set up FileReader */
-    const reader = new FileReader();
+    const reader = new FileReader(bb);
     const rABS = !!reader.readAsBinaryString;
 
     reader.onload = e => {
@@ -40,12 +47,14 @@ function App() {
       /* Update state */
 
       setData(data);
-      setCols(make_cols(ws['!ref']));
+      console.log(data);
+     // setCols(make_cols(ws['!ref']));
       
         // console.log(JSON.stringify(data, null, 2));
-        console.log(cols);
+       // console.log(cols);
       
     };
+    
 
     if (rABS) {
       reader.readAsBinaryString(file);
@@ -56,13 +65,64 @@ function App() {
     ;
   }
 
+
+  async function test() {
+     const file =  document.querySelector('input').files[0];
+   
+    console.log(file);
+
+    
+    try {
+      /* Boilerplate to set up FileReader */
+      const reader = await new FileReader();
+      const rABS = !!reader.readAsBinaryString;
+  
+      reader.onload = (e) => {
+        /* Parse data */
+        
+        const bstr = e.target.result;
+        const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array', bookVBA : true });
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const kata = XLSX.utils.sheet_to_json(ws);
+
+        setData(kata);
+        console.log(kata);
+        
+        /* Update state */
+        // this.setState({ data: data, cols: make_cols(ws['!ref']) }, () => {
+        //   console.log(JSON.stringify(this.state.data, null, 2));
+        // });
+  
+      };
+  
+      if (rABS) {
+        reader.readAsBinaryString(file);
+      }
+       else {
+        reader.readAsArrayBuffer(file);
+      };
+  } catch(err) {
+    console.log(err);
+  }
+
+    
+
+
+
+
+  }
+
   return (
     <div>
         <label htmlFor="file">Upload an excel to Process Triggers</label>
         <br />
-        <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={handleChange} />
+        <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={test} />
         <br />
-        <input type='submit' value="Process Triggers" onClick={handleFile} />
+        <input type='submit' value="Process Triggers" />
+        {data.length}
     </div>
   );
 }
